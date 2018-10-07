02fc73ab85f28014d89931fc6314f74a81359fb1
NicoGraphiste <38383445+NicoGraphiste@users.noreply.github.com>
2018-10-07 21:27:15 +0200

    Update elevel.js

--- commands/elevel.js
+++ commands/elevel.js
@@ -1,31 +1,32 @@
 const Discord = require("discord.js");
+const botconfig = require("../botconfig");
 let xp = require("../xp.json");
 
 module.exports.run = async (bot, message, args) => {
 
-    if(!xp[message.author.id]){
-    xp[message.author.id] = {
-        xp: 0,
-        level: 1
-    };
+  if(!xp[message.author.id]){
+   xp[message.author.id] = {
+     xp: 0,
+     level: 1
+  };
 }
-let curxp = xp[message.author.id].xp;
-let curlvl = xp [message.author.id].level;
-let nxtLvlXp = curlvl * 300;
-let difference = nxtLvlXp - curxp;
+  let curxp = xp[message.author.id].xp;
+  let curlvl = xp[message.author.id].level;
+  let nxtLvlXp = curlvl * 300;
+  let difference = nxtLvlXp - curxp;
 
-let elvlEmbed = new Discord.RichEmbed()
-.setAuthor(message.author.username)
-.setColor("#00ffff")
-.addField("eLevel", curlvl, true)
-.addField("XP", curxp, true)
-.addFooter(`${difference} Xp jusqu'au niveau supérieur`, message.author.displayAvatarURL);
+  let lvlEmbed = new Discord.RichEmbed()
+  .setAuthor(message.author.username)
+  .setColor("#00ffff")
+  .addField("eLevel", curlvl, true)
+  .addField("XP", curxp, true)
+  .setFooter(`${difference} XP jusqu'au niveau supérieur`, message.author.displayAvatarURL);
 
-return message.channel.send(elvlEmbed).then(msg => {msg.delete(5000)});
-
+  message.channel.send(lvlEmbed).then(msg => {msg.delete(5000)});
 
 }
 
 module.exports.help = {
-    name: "elevel"
+  name: "elevel"
+}"
 }
